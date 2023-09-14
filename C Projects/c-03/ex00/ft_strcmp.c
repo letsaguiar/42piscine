41ea513cc805b97ccdecd2dde839511291c54f51
Letícia Aguiar <letsaguiar@yandex.com>
2023-09-14 14:50:41 -0300

    c-03: add some modifications

--- C Projects/c-03/ex00/ft_strcmp.c
+++ C Projects/c-03/ex00/ft_strcmp.c
@@ -12,14 +12,14 @@
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (s1[i] || s2[i])
 	{
-		if (!s1[i] || s1[i] < s2[i])
+		if (s1[i] < s2[i])
 			return (-1);
-		else if (!s2[i] || s1[i] > s2[i])
+		if (s1[i] > s2[i])
 			return (1);
 		i++;
 	}
--- C Projects/c-03/ex01/ft_strncmp.c
+++ C Projects/c-03/ex01/ft_strncmp.c
@@ -19,9 +19,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	{
 		if (!s1[i] && !s2[i])
 			break ;
-		else if (!s1[i] || s1[i] < s2[i])
+		if (s1[i] < s2[i])
 			return (-1);
-		else if (!s2[i] || s1[i] > s2[i])
+		if (s1[i] > s2[i])
 			return (1);
 		i++;
 	}
