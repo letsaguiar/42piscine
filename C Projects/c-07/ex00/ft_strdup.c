3cf84d505a17566165290c801aecd07e9a4e8bb1
Letícia Aguiar <letsaguiar@yandex.com>
2023-09-15 09:24:58 -0300

    c-07: finish ex00

---
+++ C Projects/c-07/ex00/ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leticia-aguiar <marvin@42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 09:20:30 by leticia-aguia     #+#    #+#             */
+/*   Updated: 2023/09/15 09:24:24 by leticia-aguia    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	len = ft_strlen(src);
+	dest = malloc((len + 1) * sizeof (char));
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
