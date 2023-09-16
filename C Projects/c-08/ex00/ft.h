0a8edd566aac3c99df3054397a52733d2112f455
Letícia Aguiar <letsaguiar@yandex.com>
2023-09-16 06:48:25 -0300

    c-08: finish ex00

---
+++ C Projects/c-08/ex00/ft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leticia-aguiar <marvin@42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 06:21:19 by leticia-aguia     #+#    #+#             */
+/*   Updated: 2023/09/16 06:22:20 by leticia-aguia    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+
+void	ft_swap(int *a, int *b);
+
+void	ft_putstr(char *str);
+
+int		ft_strlen(char *str);
+
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
